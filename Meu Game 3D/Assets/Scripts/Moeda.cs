25fffadfd8dbaff11f71ab3d9345cf824c737068
protons77 <souza.diego@escolar.ifrn.edu.br>
2024-09-03 14:26:45 -0300

    script da moeda

---
+++ Meu Game 3D/Assets/Scripts/Moeda.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 50;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+            
+    }
+    
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * velocidadeGiro * Time.deltaTime);
+    }
+}
