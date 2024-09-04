8a970676193d543606a9a2db807d53e961718f8c
protons77 <souza.diego@escolar.ifrn.edu.br>
2024-09-04 11:52:58 -0300

    metade do gamemanager

--- Meu Game 3D/Assets/Scripts/GameManager.cs
+++ Meu Game 3D/Assets/Scripts/GameManager.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
+
+
+    public TextMeshProUGUI hud, MsgVitoria;
+    public int restantes;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        restantes = FindObjectsOfType<Moeda>().Length;
+
+        hud.text = $"Moedas Restantes: {restantes}";
     }
 
+    public void SubtrairMoedas(int valor)
+    {
+        restantes -= valor;
+
+        if (restantes <= 0)
+        {
+            MsgVitoria.text = "VAPOOOO";
+        }
+    }
     // Update is called once per frame
     void Update()
     {
